29041b01261d92f4727043099c7753c2f729a131
bufang <1931297+budaobu@users.noreply.github.com>
2024-12-13 12:06:57 +0800

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -245,7 +245,7 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>Joey Hu - a prompt engineer, product manager.</title>
+        <title>Joey Hu - a prompt engineer, product manager, AI coding.</title>
         <meta
           name="description"
           content="I'm Joey Hu. a prompt engineer, product manager."
@@ -254,7 +254,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Product Manager, AI Dreamer, and a Startup Co-founder.
+          Product Manager, prompt engineer, AI coding.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           I&apos;m Joey hu, a prompt engineer, product manager. Passionate about AI product, I balance my professional drive with a love for travel. I enjoy both the comfort of home and the excitement of exploring the world.

2b200a1390d40944cc3cdce49efe442fc9979a60
bufang <1931297+budaobu@users.noreply.github.com>
2024-12-14 14:24:56 +0800

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,7 @@
 import Link from 'next/link'
 import clsx from 'clsx'
+import { ChevronRight } from 'lucide-react'
 
-function ChevronRightIcon(props) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M6.75 5.75 9.25 8l-2.5 2.25"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
 
 export function Card({ as: Component = 'div', className, children }) {
   return (
@@ -59,7 +48,7 @@ Card.Cta = function CardCta({ children }) {
       className="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
     >
       {children}
-      {/* <ChevronRightIcon className="ml-1 h-4 w-4 stroke-current" /> */}
+      <ChevronRight size={16} strokeWidth={1.5} className="ml-1 h-4 w-4 stroke-current" />
     </div>
   )
 }

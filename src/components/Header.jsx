efae9ff6dee553ddb51efe2d403ab47ea0cf22fd
budaohu <1931297+budaobu@users.noreply.github.com>
2024-12-15 20:58:41 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import clsx from 'clsx'
 

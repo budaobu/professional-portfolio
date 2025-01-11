8aade406f6ffb1115d1e8079f766f35c7c018093
bufang <aiguai131458+github@gmail.com>
2025-01-11 22:34:07 +0800

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -227,12 +227,13 @@ function Photos() {
                 src={image}
                 alt=""
                 sizes="(min-width: 640px) 18rem, 11rem"
-                className="absolute inset-0 h-full w-full object-cover"
+                className="absolute inset-0 h-full w-full object-cover rounded-xl sm:rounded-2xl"
               />
             </div>
           ))}
           </div>
-        
+        <div className="absolute left-0 top-0 bottom-0 w-1/12 bg-gradient-to-r from-white to-transparent dark:from-zinc-900"></div>
+        <div className="absolute right-0 top-0 bottom-0 w-1/12 bg-gradient-to-l from-white to-transparent dark:from-zinc-900"></div>
       </div>
     </div>
   )

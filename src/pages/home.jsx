df65459594b22e49d19c9706cfe2856af1c9693b
bufang <aiguai131458+github@gmail.com>
2025-01-11 22:28:16 +0800

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -232,8 +232,7 @@ function Photos() {
             </div>
           ))}
           </div>
-        <div className="absolute left-0 top-0 bottom-0 w-1/12 bg-gradient-to-r from-white to-transparent dark:from-zinc-900"></div>
-        <div className="absolute right-0 top-0 bottom-0 w-1/12 bg-gradient-to-l from-white to-transparent dark:from-zinc-900"></div>
+        
       </div>
     </div>
   )

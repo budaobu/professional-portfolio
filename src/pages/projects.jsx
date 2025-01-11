02fd03a4fee4d135d0ae528a040dfe4dc4894771
bufang <aiguai131458+github@gmail.com>
2025-01-11 11:58:30 +0800

    Update projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -94,7 +94,7 @@ export default function Projects() {
               </h2>
               <Card.Description>{project.description}</Card.Description>
               <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <Link size={24} strokeWidth={1.5} className="h-6 w-6 flex-none" />
+                <Link size={20} strokeWidth={1.5} className="h-6 w-6 flex-none" />
                 <span className="ml-2">{project.link.label}</span>
               </p>
             </Card>

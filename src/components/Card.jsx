0025eda50b9dbe1811a39ed527382dc64aabede6
bufang <aiguai131458+github@gmail.com>
2025-01-16 15:10:16 +0800

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -85,7 +85,7 @@ Card.Eyebrow = function CardEyebrow({
 Card.Image = function CardImage({ src, alt }) {
   return (
     <div className="absolute top-1/2 right-2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-      <img
+      <Image
         src={src}
         alt={alt}
         className="w-24 h-24 object-cover transform group-hover:scale-110 transition-transform duration-300"

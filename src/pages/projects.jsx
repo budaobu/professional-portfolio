45d649a171d965bfc2bf0bd8cddb1da3f19e503c
bufang <aiguai131458+github@gmail.com>
2025-01-16 15:15:18 +0800

    Update projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -16,15 +16,6 @@ const projects = [
       label: 'Watermark',
     },
   },
-  {/* {
-    name: 'Free Open Graph Image Generator',
-    description:
-      "Use this free Open Graph image generator to create beautiful images for your website, blog, or X (Twitter) profile with zero effort.",
-    link: {
-      href: 'https://og.dakaiai.app/',
-      label: 'Open Graph Image',
-    },
-  }, */}
   {
     name: 'Fuel or Elec Car',
     description:

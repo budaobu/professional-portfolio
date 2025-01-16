7d1688f5e777a630b40e2b20efce579282c051d0
bufang <aiguai131458+github@gmail.com>
2025-01-16 14:55:56 +0800

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -252,10 +252,10 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Cyber refugee.
+          Hi, I&apos;m Joey Hu, a cyber refugee.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          I&apos;m Joey hu, a prompt engineer, product manager. Passionate about AI product, I balance my professional drive with a love for travel. I enjoy both the comfort of home and the excitement of exploring the world.
+          I am trying to find a balance between life and work. I enjoy the comfort of home but also the excitement of exploring the world.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink

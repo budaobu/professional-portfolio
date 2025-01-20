d30bd2d4637085e22e1c3ce261e32efb72d55bfe
bufang <aiguai131458+github@gmail.com>
2025-01-20 17:14:46 +0800

    Update ArticleLayout.jsx

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { MDXProvider } from '@mdx-js/react'
-import { CornerLeftUp } from 'lucide-react'
+import { ArrowBendLeftUp } from '@phosphor-icons/react'
 import { Container } from '@/components/Container'
 import { formatDate } from '@/lib/formatDate'
 import { Prose } from '@/components/Prose'
@@ -35,7 +35,7 @@ export function ArticleLayout({
                 aria-label="Go back to articles"
                 className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:mb-0 lg:-mt-2 xl:-top-1.5 xl:left-0 xl:mt-0"
               >
-                <CornerLeftUp size={16} strokeWidth={1.5} className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+                <ArrowBendLeftUp size={16} strokeWidth={1.5} className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
               </button>
             )}
             <article>
